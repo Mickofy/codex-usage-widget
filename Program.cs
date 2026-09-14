403c66aa152f1ca74fbe1d1c37bc8056b68b340a
Mickofy <mabwaje@yahoo.com>
2026-09-15 02:56:36 +0800

    feat: add application entry point

---
+++ Program.cs
@@ -0,0 +1,35 @@
+namespace CodexUsageWidget;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+
+        Application.ThreadException += (_, e) =>
+        {
+            AppLog.Write("UI exception", e.Exception);
+        };
+
+        AppDomain.CurrentDomain.UnhandledException += (_, e) =>
+        {
+            if (e.ExceptionObject is Exception ex)
+                AppLog.Write("Unhandled exception", ex);
+        };
+
+        try
+        {
+            Application.Run(new TrayApplicationContext());
+        }
+        catch (Exception ex)
+        {
+            AppLog.Write("Fatal startup error", ex);
+            MessageBox.Show(
+                $"{ex.Message}\n\nLog:\n{AppLog.LogPath}",
+                "Codex Usage Widget",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+        }
+    }
+}
